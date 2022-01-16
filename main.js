13d31a4c105693b5cc4e789a7112c4ed358d1194
sanaycode <88113041+sanaycode@users.noreply.github.com>
2022-01-16 17:18:14 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function crane(){
+    window.location="crane.html";
+}
+function waterbomb(){
+    window.location="waterbomb.html";
+}
+function frog(){
+    window.location="frog.html";
+}
+function pinwheel(){
+    window.location="pinwheel.html";
+}
+function star(){
+    window.location="star.html";
+}
+function swan(){
+    window.location="swan.html";
+}
